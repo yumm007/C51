b603d68ea03b8a6f86548a1ee85dba6075073a04
yumm_007 <yumm007@gmail.com>
2011-12-06 01:07:17 +0800

    块读写还没有成功

--- spi.c
+++ spi.c
@@ -1,6 +1,7 @@
 #include <STC12C5A60S2.H>
 #include "lib.h"
 #include "usart.h"
+#include <string.h>
 #include <stdio.h>
 
 sbit MISO 	= P1 ^ 6;
@@ -68,13 +69,104 @@ static u16 spi_device_id(void) {
 	return ret;
 }
 
+
+
+#define WRITE_ENABLE 	0x06
+#define WRITE_DISABLE	0x04
+#define READ_STA1		0x05
+#define READ_STA2		0x35
+#define WRITE_STA		0x01
+#define SECTOR_ERASE	0x20
+#define	FAST_READ		0x0B
+#define	PAGE_PROGRAM	0x02
+
+#define SPI_BUSY		BIT0
+
+static u8 spi_sta(void) {
+	u8 ret = 0;
+	SS = 0;
+	spi_send_byte(READ_STA1);
+	ret = spi_recv_byte();
+	SS = 1;
+	return ret;
+}
+
+int spi_read(u16 addr, char *dst, u16 buf_size) {
+	SS = 0;
+	spi_send_byte(0x03);
+	spi_send_byte(addr & 0xff0000);
+	spi_send_byte(addr & 0x00ff00);
+	spi_send_byte(addr & 0x0000ff);
+	spi_send_byte(SPI_DUMMY);
+	
+	while (buf_size--)
+		*dst++ = spi_recv_byte();
+
+	SS = 1;	
+}
+
+//一次最多写256个字节
+//一次最少擦写4K
+//写操作为完成时BUSY置1,闲时硬件自动清0
+//static char xdata page_buf[256];
+
+#define WEN 	spi_write_opt(TRUE)
+#define WDIS	spi_write_opt(FALSE)
+static void spi_write_opt(bool val) {
+	SS = 0;
+	spi_send_byte(val ? WRITE_ENABLE : WRITE_DISABLE);
+	SS = 1;	
+}
+
+int spi_write(u16 addr, const char *src, u16 len) {
+	u16 i = 0;
+	u8 ret = 1;
+	//memset(page_buf, 0, sizeof(page_buf));
+	WEN;
+
+	SS = 0;
+	spi_send_byte(SECTOR_ERASE);
+	spi_send_byte(addr & 0xff0000);
+	spi_send_byte(addr & 0x00ff00);
+	spi_send_byte(addr & 0x0000ff);
+
+	do {
+	   ret = spi_sta();
+	} while (ret & SPI_BUSY);	
+
+	SS = 0;
+	spi_send_byte(PAGE_PROGRAM);
+	spi_send_byte(addr & 0xff0000);
+	spi_send_byte(addr & 0x00ff00);
+	spi_send_byte(addr & 0x0000ff);	
+
+	for (i = 0; i < 256; i++)
+		spi_send_byte(i < len ? src[i] : 0);
+	
+	//delay_ms(100);
+
+	do {
+	   ret = spi_sta();
+	} while (ret & SPI_BUSY);
+
+	SS = 1;
+	WDIS;
+}
+
 void spi_deinit(void) {
 	DISABLE_SPI;
 }
 
 void spi_init(void) {
-	SS = 1;
+	char xdata tmp[50] = "aaaa aaaa aaaa\n";
+
 	SPCTL 	= 	SPI_DEF_VAL;
 	SPSTAT 	=  	SPIF | ECOL;
+
 	printf("\"ef16\"\t.spi device id:0x%x\n", spi_device_id());
+
+	//spi_write(0, "this line will be write to spi flash.\n", strlen("this line will be write to spi flash.\n"));
+	//delay_ms(1000);
+	spi_read(0, tmp , strlen("this line will be write to spi flash.\n"));
+	printf("%s", tmp);
 }
