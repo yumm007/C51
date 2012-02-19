d4fead6d296f49a2c540942660725c8ddd45749a
yumm_007 <yumm007@gmail.com>
2012-02-19 17:40:36 +0800

    整合字库代码

--- font.c
+++ font.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include "lib.h"
+#include "spi.h"
 
 typedef enum {
     FONT_12	= 12,
@@ -8,13 +10,13 @@ typedef enum {
     FONT_MAX	= FONT_24,
 } FONT_SIZE_T;
 
-typedef enum {
-    ASC_12_OFFS = 0,
-    ASC_16_OFFS	= ASC_12_OFFS + 1536,
-    ASC_24_OFFS	= ASC_16_OFFS + 4096,
-    HZK_16_OFFS	= ASC_24_OFFS + 12288,
-    HZK_24_OFFS	= HZK_16_OFFS + 267616,
-} FONT_OFFSET_T;
+//typedef enum {
+#define    ASC_12_OFFS  0
+#define    ASC_16_OFFS	 (ASC_12_OFFS + 1536)
+#define    ASC_24_OFFS	 (ASC_16_OFFS + 4096)
+#define    HZK_16_OFFS	 (ASC_24_OFFS + 12288)
+#define    HZK_24_OFFS	 (HZK_16_OFFS + 267616)
+//} FONT_OFFSET_T;
 
 typedef enum {
     ASC_12,
@@ -47,65 +49,19 @@ static FONT_TYPE_T get_word_type(FONT_SIZE_T size, unsigned char is_hz) {
 	    ret = FONT_ERR;
 	    break;
     }
-    fprintf(stderr, "word type is %d\t", ret);
+
     return ret;
 }
 
-//row, line始终指向下一个空白位置,可能换行也可能跳到行首
-static void cal_lca(FONT_TYPE_T font_type, int *row, int *lines) {
-    int row_1 = *row, line_1 = *lines;
-    int font_size_r, font_size_l; 
-    switch (font_type) {
-	case ASC_12:
-	    font_size_r = 8;
-	    font_size_l = 12;
-	    break;
-	case ASC_16:
-	    font_size_r = 8;
-	    font_size_l = 16;
-	    break;
-	case ASC_24:
-	    font_size_r = 16;
-	    font_size_l = 24;
-	    break;
-	case HZK_16:
-	    font_size_r = 16;
-	    font_size_l = 16;
-	    break;
-	case HZK_24:
-	    font_size_r = 24;
-	    font_size_l = 24;
-	    break;
-	default:
-	    font_size_r = 0;
-	    font_size_l = 0;
-	    break;
-    }
-
-    row_1 += font_size_r + LCD_LINE_EMPTY;
-    if (row_1 >= LCD_ROW) {
-	row_1 = 0;
-	line_1 += font_size_l + LCD_LINE_EMPTY;
-	if (line_1 >= LCD_LINE)
-	    line_1 = 0;
-    }
-
-    *row = row_1;
-    *lines = line_1;
-}
 
 ////从字库中取出当前字的点阵, 并返回总共的字节数
 static void SST25_R_BLOCK(int offset, unsigned char *bit_buf, int len) {
-    FILE *fp = fopen("new_font.bin", "r");
-    if (fp == NULL) {
-	fprintf(stderr, "open font.bin error.\n");
-    }
-    fseek(fp, offset, SEEK_SET);
-    if (fread(bit_buf, 1, len, fp) < len)
-	fprintf(stderr, "fread %s error, try to read %d byte, offset %d.\n", "font.bin", len, offset);
-    else 
-	fprintf(stderr, "OK. try to read %d byte.\n",  len);
-    fclose(fp);
+//    FILE *fp = fopen("font.bin", "r");
+//    fseek(fp, offset, SEEK_SET);
+//    fread(bit_buf, 1, len, fp);
+//    fclose(fp);
+	spi_read(offset, bit_buf, len);
+	return;
 }
 
 static int get_bitmap(FONT_TYPE_T font_type, unsigned char *bit_buf, const unsigned char *str) {
@@ -177,42 +133,92 @@ static void send_8bit(FONT_TYPE_T font_type, unsigned char tmp) {
 
 }
 
-static void set_lcd_row_line(FONT_SIZE_T size, int row, int lines) {
-    //extern void LCD_Lib_SetXY(unsigned short sax, unsigned short say, unsigned short eax, unsigned short eay);
-    //LCD_Lib_SetXY(lines, row, lines + size, row + size);
-    printf("\n");
+
+//设置屏幕起始和结束为止,并将
+//row, line指向下一个空白位置,可能换行也可能跳到行首
+static void set_lcd_row_line(FONT_TYPE_T font_type, int *rows, int *lines) {
+    extern void MainLCD_Window_Set(unsigned short sax, unsigned short say, unsigned short eax, unsigned short eay);
+	
+	int font_size_r, font_size_l, cur_row = *rows, cur_line = *lines, next_row, next_line;
+
+	switch (font_type) {
+	case ASC_12:
+	    font_size_r = 8;
+	    font_size_l = 12;
+	    break;
+	case ASC_16:
+	    font_size_r = 8;
+	    font_size_l = 16;
+	    break;
+	case ASC_24:
+	    font_size_r = 16;
+	    font_size_l = 24;
+	    break;
+	case HZK_16:
+	    font_size_r = 16;
+	    font_size_l = 16;
+	    break;
+	case HZK_24:
+	    font_size_r = 24;
+	    font_size_l = 24;
+	    break;
+	default:
+	    font_size_r = 0;
+	    font_size_l = 0;
+	    break;
+    }
+
+	if (cur_row + font_size_r >= LCD_ROW) {
+		cur_row = 0;
+		cur_line += font_size_l + LCD_LINE_EMPTY;
+	}
+
+	if (cur_line >= LCD_LINE) {
+		cur_line = 0;
+		cur_row = 0;
+	}
+
+	next_row = 	 cur_row + font_size_r;
+	next_line =  cur_line + font_size_l;
+
+	MainLCD_Window_Set(cur_line, cur_row, next_line, next_row);
+
+
+	next_row += LCD_LINE_EMPTY;
+	next_line += LCD_LINE_EMPTY;
+
+    *rows = next_row;
+	*lines = next_line;
+
 }
 
 void lcd_print(FONT_SIZE_T size, int row, int lines, const unsigned char *str) {
-    unsigned char tmp;		//指向当前字符
-    char is_hz;
+    unsigned char is_hz;
     unsigned char bit_buf[FONT_MAX * (FONT_MAX/8)];
     int i, j;
     FONT_TYPE_T font_type;
 
     while (*str != '\0') {
-	is_hz = (*str) > 0xa0 ? 1 : 0;	//判断是否为汉字
-	//设置屏幕输出的起始位置
-	set_lcd_row_line(size, row, lines);
-
-	//返回字体类型
-	font_type = get_word_type(size, is_hz);
-
-	//从字库中取出当前字的点阵, 并返回总共的字节数
-	j = get_bitmap(font_type, bit_buf, str);
-
-	for (i = 0; i < j; i++) 
-	    //从低位到高位，位为0表示描背景色，位为1表示为字体颜色；
-	    send_8bit(font_type, bit_buf[i]);
-
-	//row, line始终指向下一个空白位置,可能换行也可能跳到行首
-	str = is_hz ? str + 2 : str + 1;	//指向下一个字符
-	cal_lca(font_type, &row, &lines);
+		is_hz = (*str) > 0xa0 ? 1 : 0;	//判断是否为汉字	
+		//返回字体类型
+		font_type = get_word_type(size, is_hz);
+		//设置屏幕输出的起始位置
+		set_lcd_row_line(font_type, &row, &lines);
+	
+		//从字库中取出当前字的点阵, 并返回总共的字节数
+		j = get_bitmap(font_type, bit_buf, str);
+	
+		for (i = 0; i < j; i++) 
+		    //从低位到高位，位为0表示描背景色，位为1表示为字体颜色；
+		    send_8bit(font_type, bit_buf[i]);
+	
+		//row, line始终指向下一个空白位置,可能换行也可能跳到行首
+		str = is_hz ? str + 2 : str + 1;	//指向下一个字符
     }
 
 }
 
-int main(void) {
+int print_test(void) {
     unsigned char tmp[] = {0xCE, 0xD2, 0xC3, 0xC7, 0x00 }; //汉字GB2312 "我们"
     lcd_print(FONT_12, 0, 0, "abced");
     lcd_print(FONT_16, 0, 0, "abced");
--- spi.h
+++ spi.h
@@ -5,8 +5,9 @@
 
 void spi_init(void);
 void spi_deinit(void);
-
 u8 spi_send_byte(u8 val);
 u8 spi_recv_byte(void);
+
+int spi_read(u32 addr, char *dst, u32 buf_size);
 
 #endif
